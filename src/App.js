e38e71731ce974cb6f78665e1aea692fe66be553
mapper895 <mapper.895.mp@gmail.com>
2020-12-20 11:54:31 -0600

    React Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import AboutUS from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUS />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUS />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
