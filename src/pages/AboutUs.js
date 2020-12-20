34ff7d407f7d28c192ea5bd1b4ffebecfdf2af26
mapper895 <mapper.895.mp@gmail.com>
2020-12-19 18:51:14 -0600

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,15 @@
 import React from "react";
 // Page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUS = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />)
+    </>
+  );
 };
 
 export default AboutUS;
