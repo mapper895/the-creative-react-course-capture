18fa749abe573a722e9054f90013bae1f3b22471
mapper895 <mapper.895.mp@gmail.com>
2020-12-19 20:13:45 -0600

    6. Services Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import home1 from "../img/home1.png";
 // Styles
 import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
@@ -22,7 +23,7 @@ const AboutSection = () => {
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
-          ahve profesionals with amazings skills.
+          have profesionals with amazings skills.
         </p>
         <button>Contact Us</button>
       </Description>
@@ -32,34 +33,5 @@ const AboutSection = () => {
     </About>
   );
 };
-
-// Styled Component
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
